00f937a45a24e2827184eb78f39291d390f2a4d0
golam mostafa <gmnayeem25@gmail.com>
2023-02-18 20:29:01 +0600

    contact form create

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './app.css';
+import Contact from './components/Contact'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <h1>Contact Page</h1>
+      <Contact />
+    </div>
   )
 }
 
